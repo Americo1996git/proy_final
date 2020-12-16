a6c2e2401dd1ab3d728faa66deed03e76b56e63d
Daniel <wanopacers@mesoamericana.edu.gt>
2020-12-16 16:10:22 -0600

    Se agrearon las configuraciones para desplegar la aplicacion - se utilizo Digita Ocean - se utilizo un Contenedro de Docker

--- muro_notas/settings.py
+++ muro_notas/settings.py
@@ -74,12 +74,26 @@ WSGI_APPLICATION = 'muro_notas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 
 
 # Password validation
